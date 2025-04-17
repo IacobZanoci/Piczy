041e61f61274af49ed32df286eb8c72a5d83a801
IacobZanoci <114911332+IacobZanoci@users.noreply.github.com>
2025-04-17 22:02:24 +0300

    Initial commit

---
+++ Piczy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Piczy
+//
+//  Created by Iacob Zanoci on 17.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
