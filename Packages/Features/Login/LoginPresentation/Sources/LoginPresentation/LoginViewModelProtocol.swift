8dbee0154fc7485b6dd7a6e81948980fe5eb1e44
IacobZanoci <114911332+IacobZanoci@users.noreply.github.com>
2025-04-27 23:55:44 +0300

    feat: Add LoginPresentation structure and related files

--- Packages/Features/Login/LoginPresentation/Sources/LoginPresentation/LoginViewModelProtocol.swift
+++ Packages/Features/Login/LoginPresentation/Sources/LoginPresentation/LoginViewModelProtocol.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+@MainActor
+public protocol LoginViewModelProtocol {
+    
+    // MARK: - Properties
+    
+    var onEmailErrorChanged: ((String?) -> Void)? { get set }
+    var onPasswordErrorChanged: ((String?) -> Void)? { get set }
+    var onLoginButtonEnabled: ((Bool) -> Void)? { get set }
+    
+    func onEmailChanged(to email: String)
+    
+    func onPasswordChanged(to password: String)
+    
+    /// Event triggered when the user wants to log in.
+    func onLogin()
+    
+    ///Event triggered when the user forgot password.
+    func onForgotPassword()
+    
+    /// Event triggered when the user attempts to create a new account.
+    func onCreateAccount()
+}
