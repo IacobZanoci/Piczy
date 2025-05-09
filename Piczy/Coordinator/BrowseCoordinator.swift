c59d1dc5ea813624db71b9eaa82f59577f689048
IacobZanoci <114911332+IacobZanoci@users.noreply.github.com>
2025-05-09 15:52:05 +0300

    feat: Implement Coordinator pattern for the app navigation

---
+++ Piczy/Coordinator/BrowseCoordinator.swift
@@ -0,0 +1,21 @@
+//
+//  BrowseCoordinator.swift
+//  Piczy
+//
+//  Created by Iacob Zanoci on 09.05.2025.
+//
+
+import Foundation
+import UIKit
+
+final class BrowseCoordinator: CoordinatorProtocol {
+    var navigationController: UINavigationController
+    
+    init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        // Navigate to the main screen after successful authentication
+    }
+}
