e6d3f9898eff95d0f350119c49e62e256a431f04
IacobZanoci <114911332+IacobZanoci@users.noreply.github.com>
2025-05-09 15:11:07 +0300

    chore: Update SceneDelegate to navigate to Login View

--- Piczy/SceneDelegate.swift
+++ Piczy/SceneDelegate.swift
@@ -9,6 +9,7 @@ import UIKit
 import WelcomePresentation
 import SignUpPresentation
 import LoginPresentation
+import CredentialsValidator
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
@@ -26,6 +27,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(windowScene: windowScene)
         let welcomeVC = makeWelcomeViewController()
         let navController = UINavigationController(rootViewController: welcomeVC)
+        window?.rootViewController = makeWelcomeViewController()
         window?.rootViewController = navController
         window?.makeKeyAndVisible()
     }
@@ -34,25 +36,60 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     private func makeWelcomeViewController() -> UIViewController {
         let viewModel = WelcomeViewModel(
-            onCreateAccount: { [weak self] in
-                self?.navigateToSignUp()
+            onCreateAccount: {
+                self.navigateToSignUp()
             },
             onLogIn: {
-                // Navigate to Log In screen
+                self.navigateToLogin()
             }
         )
         return WelcomeViewController(viewModel: viewModel)
     }
     
+    private func makeLoginViewController() -> UIViewController {
+        let viewModel = LoginViewModel(
+            onLogin: {
+                print("On login button pressed")
+            },
+            onForgotPassword: {
+                print("On forgot password button pressed")
+            },
+            onCreateAccount: {
+                print("On create account button pressed")
+            },
+            credentialsValidator: CredentialsValidator()
+        )
+        return LoginViewController(viewModel: viewModel)
+        
+    }
+    
     private func navigateToSignUp() {
         let viewModel = SignUpViewModel(
             onSignUp: {
-                print("success")
+                print("SignUp succeeded")
             }
         )
         let signUpVC = SignUpViewController(viewModel: viewModel)
         (window?.rootViewController as? UINavigationController)?
             .pushViewController(signUpVC, animated: true)
+    }
+    
+    private func navigateToLogin() {
+        let viewModel = LoginViewModel(
+            onLogin: {
+                print("Login button pressed")
+            },
+            onForgotPassword: {
+                print("Forgot password button pressed")
+            },
+            onCreateAccount: {
+                print("Create account button pressed")
+            },
+            credentialsValidator: CredentialsValidator()
+        )
+        let loginVC = LoginViewController(viewModel: viewModel)
+        (window?.rootViewController as? UINavigationController)?
+            .pushViewController(loginVC, animated: true)
     }
 }
 
